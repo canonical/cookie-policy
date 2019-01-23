ad29614a6ac3f44e2ca7f14153d5f3580a3bce0b
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-01-23 09:51:08 +0000

    Refactor Gulpfile to use Gulp v4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,78 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var sassLint = require("gulp-sass-lint");
-var jshint = require("gulp-jshint");
-var babel = require("gulp-babel");
+const babel = require('gulp-babel');
+const del = require('del');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
+const sassLint = require('gulp-sass-lint');
 
-gulp.task("babel", function() {
-  gulp
-    .src("./src/js/**/*.js")
-    .pipe(
-      babel({
-        presets: ["es2015"]
-      })
-    )
-    .pipe(gulp.dest("./build/js"));
-});
+const { series, parallel, watch } = require('gulp');
 
-gulp.task("lint:js", function() {
+// Remove ./build folder
+const clean = () => {
+  return del(['build']);
+};
+
+// Lint all *js files with jshint
+const lintJs = () => {
   return gulp
-    .src("./src/js/**/*.js")
-    .pipe(jshint())
-    .pipe(jshint.reporter("default"));
-});
+    .src('./src/js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+};
 
-gulp.task("lint:sass", function() {
+// Lint all *scss files with sassLint
+const lintScss = () => {
   return gulp
-    .src("./src/sass/**/*.scss")
+    .src('./src/sass/**/*.scss')
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError());
-});
+};
 
-gulp.task("sass", function() {
+// Compile Scss files to CSS
+const buildScss = () => {
   return gulp
-    .src("./src/sass/**/*.scss")
+    .src('./src/sass/**/*.scss')
     .pipe(
       sass({
-        outputStyle: "compressed",
-        includePaths: "node_modules"
-      }).on("error", sass.logError)
+        outputStyle: 'compressed',
+        includePaths: './node_modules',
+      }).on('error', sass.logError)
     )
-    .pipe(gulp.dest("./build/css"));
-});
+    .pipe(gulp.dest('./build/css'));
+};
 
-gulp.task("test", ["lint:sass", "lint:js"]);
+// Transpile JS with Babel
+const buildJs = done => {
+  gulp
+    .src('./src/js/**/*.js')
+    .pipe(
+      babel({
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              useBuiltIns: 'entry',
+            },
+          ],
+        ],
+      })
+    )
+    .pipe(gulp.dest('./build/js'));
+  done();
+};
 
-gulp.task("build", ["babel", "sass"]);
+// Run watch tasks in development on Js and Scss files to regenerate when changed
+const dev = () => {
+  watch('./src/sass/**/*.scss', buildScss);
+  watch('./src/js/**/*.js', buildJs);
+};
 
-gulp.task("dev", function() {
-  gulp.watch("./src/sass/**/*.scss", ["sass"]);
-  gulp.watch("./src/js/**/*.js", ["babel"]);
-});
+// Expose public tasks
+exports.clean = clean;
+exports.dev = dev;
+exports.lint = parallel(lintJs, lintScss);
+exports.build = series(clean, parallel(lintJs, lintScss, buildJs, buildScss));
