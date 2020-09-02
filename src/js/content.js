05bd19be8d215b86bf03e0fe7301e42b43415d0b
Anthony Dillon <me@anthonydillon.com>
2020-09-02 15:25:13 +0100

    Update the copy to match option 3

--- src/js/content.js
+++ src/js/content.js
@@ -3,21 +3,21 @@ export const controlsContent = [
     id: 'essential',
     title: 'Essential',
     description:
-      "Essential cookies enable the site's core functionality such as navigation, access to secure areas, video players and payments. The site cannot function properly without these cookies; they can only be disabled by changing your browser preferences.",
+      "Enables the site's core functionality, such as navigation, access to secure areas, video players and payments. The site cannot function properly without these cookies; they can only be disabled by changing your browser preferences.",
     showSwitcher: false,
   },
   {
     id: 'performance',
     title: 'Performance',
     description:
-      'Performance cookies help us to improve our site by collecting and reporting information on its usage (for example, which of our site pages are most frequently visited).',
+      'Collects information on site usage, for example, which pages are most frequently visited.',
     showSwitcher: true,
   },
   {
     id: 'functionality',
     title: 'Functionality',
     description:
-      'These are used to recognise you when you return to our site. This enables us to personalise our content for you, greet you by name and remember your preferences. We also use social media cookies to allow you to share pages on social networks.',
+      'Recognises you when you return to our site. This enables us to personalise content, greet you by name, remember your preferences, and helps you share pages on social networks.',
     showSwitcher: true,
   },
 ];
--- src/js/manager.js
+++ src/js/manager.js
@@ -11,12 +11,14 @@ export class Manager {
       <div class="p-modal" id="modal">
         <div class="p-modal__dialog" role="dialog" aria-labelledby="modal-title" aria-describedby="modal-description">
           <header class="p-modal__header">
-            <h2 class="p-modal__title" id="modal-title">Tracking preferences</h2>
+            <h2 class="p-modal__title" id="modal-title">Tracking choices</h2>
           </header>
-          <p id="modal-description">We use technologies such as cookies on our site to recognise visitors and remember their preferences, enhance user experience, personalise content and ads, provide social media features, and to measure campaign effectiveness and analyse site traffic. Please review and select which types of trackers you consent to the use of by us and third parties on our site. </p>
-          <p>For further details or to change your consent choices at any time see our cookie policy.</p>
-          <p><button class="p-button--positive u-no-margin--bottom js-close">Accept recommended settings</button></p>
-          <p>This will automatically switch all toggles below to "ON".</p>
+          <p id="modal-description">We use cookies to recognise visitors and remember your preferences.</p>
+          <p>They enhance user experience, personalise content and ads, provide social media features, measure campaign effectiveness, and analyse site traffic.</p>
+          <p>Select the types of trackers you consent to, both by us, and third parties.</p>
+          <p>Visit <a href="https://ubuntu.com/legal/data-privacy#cookies">cookie policy</a> to learn more.</p>
+          <p><button class="p-button--positive u-no-margin--bottom js-close">Accept all</button></p>
+          <p>This will switch all toggles "ON".</p>
           <hr />
           <div class="controls"></div>
           <button class="p-button--neutral js-save-preferences">Save preferences</button>
