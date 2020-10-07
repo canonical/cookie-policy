1e574e61ba7fcbc2d70ab8868520a31a74a73d36
Anthony Dillon <me@anthonydillon.com>
2020-10-07 14:54:34 +0100

    Expire the cookie after a year instead of a day

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 export const setCookie = (value) => {
   const d = new Date();
-  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
   const expires = 'expires=' + d.toUTCString();
   const samesite = 'samesite=lax;';
   document.cookie =
